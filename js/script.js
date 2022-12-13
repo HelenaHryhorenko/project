1252f1db8f41df2f9dc13f818f37c1deed9b3cef
Elena Hryhorenko <grigorenkohelena1999@gmail.com>
2022-12-13 18:26:42 +0100

    1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,93 @@
-console.log(1);
+"use strict";
+
+let n = 5;
+
+while ( n <= 10 ){
+    console.log(n);
+    n++;
+}
+
+
+for (let i = 20; i >= 12; i--){
+    if (i===13) {
+        break;
+    }
+    console.log(i);
+}
+
+for (let i = 2; i <= 10; i++) {
+    if ( i % 2 === 0){
+        console.log(i)
+    }
+}
+
+for (let i = 2; i <= 16; i++) {
+    if (i % 2 === 0) {
+        continue;
+    } else {
+        console.log(i);
+    }
+}
+
+let i = 2;
+while ( i <= 16 ) {
+    if (i % 2 === 0) {
+        i++;
+        continue;
+    } else {
+        console.log(i);
+    }
+    i++;
+}
+    const arrayOfNumbers = [];
+
+    for (let i = 5; i < 11; i++) {
+        arrayOfNumbers[i - 5] = i;
+    }
+
+    console.log(arrayOfNumbers);
+    return arrayOfNumbers;
+
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+
+    for ( let i = 0; i < arr.length; i++) {
+        result[i] = arr[i];
+    }
+    console.log (result);
+    return result;
+
+    for (let i = 0; i < data.length; i++) {
+        if (typeof(data[i]) === 'number') {
+            data[i] = data[i] * 2;
+        } else if (typeof(data[i]) === 'string') {
+            data[i] = `${data[i]} - done`;
+        }
+    }
+
+    console.log(data);
+
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
+
+     for ( let i = 1; i <= data.length; i++) {
+        result [i - 1] = data[data.length - i]
+    }
+    console.log (result);
+    return result;
+
+
+    const lines = 5;
+    let result = '';
+
+    for (let i = 0; i <= lines; i++) {
+        for (let j = 0; j <= lines-i; j++) {
+            result += " "
+        }
+        for (let j = 0; j < 2 * i + 1; j++) {
+            result += "*";
+        }
+        result += "\n";
+    }
+    console.log (result);
+    return result;
